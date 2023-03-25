9209209f59f6e473559631dc05a60205351182c2
Anton Src <anton.source@gmail.com>
2023-03-25 17:05:13 +0700

    add localStorage

--- src/log.js
+++ src/log.js
@@ -1,4 +1,9 @@
-changeFirstTD();
+window.onload = function() {
+    changeFirstTD();
+}
+
+
+
 
 function changeFirstTD() {
     console.log("CALL function " + arguments.callee.name);
@@ -21,7 +26,7 @@ function addNewEvent() {
     let editPl = document.getElementById("editorPlace");
     let editMainPl = document.getElementById("editorMainPlace");
     editPl.style.display = "block";
-    editMainPl.innerHTML = "дата\nописание";
+    // editMainPl.innerHTML = "дата\nописание";
 }
 
 function eventOk() {
@@ -29,6 +34,63 @@ function eventOk() {
 
     let editPl = document.getElementById("editorPlace");
     editPl.style.display = "none";
+}
+
+
+
+
+
+
+let stLocal = window.localStorage;
+let txtDate = document.getElementById("txtDate");
+let txtType = document.getElementById("txtType");
+let txtTime = document.getElementById("txtTime");
+
+let btnSave = document.getElementById("btnSave");
+
+function btnSaveLoc() { 
+    console.log("CALL function " + arguments.callee.name);
+
+    if ((txtDate.value) && (txtType.value) && (txtTime.value)) {
+        let dateType = txtDate.value + "_:_" + txtType.value;
+        let newTime = "";
+        if (stLocal.getItem(dateType)) {
+            console.log(stLocal.getItem(dateType));
+            newTime = Number(stLocal.getItem(dateType)) + Number(txtTime.value);
+        }
+        else {
+            newTime = txtTime.value;
+
+        }
+
+        
+        stLocal.setItem(dateType, newTime);
+    //   stLocal.setItem("content", txtContent.value); 
+    //   btnLoad.disabled = false; 
+    } 
+
+
+
+
+
+
+
+// var txtContent = document.getElementById("txtContent"); 
+// var btnLoad = document.getElementById("btnLoad"); 
+// btnLoad.disabled = !((stLocal.getItem("title")) && 
+//                      (stLocal.getItem("content"))); 
+
+// btnSave.addEventListener("click", function() { 
+//   if ((txtDate.value) && (txtContent.value)) { 
+//     stLocal.setItem("title", txtDate.value); 
+//     stLocal.setItem("content", txtContent.value); 
+//     btnLoad.disabled = false; 
+//   } 
+// }, false); 
+// btnLoad.addEventListener("click", function() { 
+//   txtDate.value = stLocal.getItem("title"); 
+//   txtContent.value = stLocal.getItem("content"); 
+// }, false); 
 }
 
 
