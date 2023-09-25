f9bc3e4e0b9f0b20ffb731fb94a5839ba0c41439
Anton Src <anton.source@gmail.com>
2023-09-25 22:23:45 +0700

    fix error after set time in event 12

--- src/main.js
+++ src/main.js
@@ -156,7 +156,7 @@ let y = 0;
 inputEvent.addEventListener('change', e => {
     errorMessage.style.display = "block";
     // console.log(inputEvent.options[inputEvent.selectedIndex].text)
-    errorMessage.textContent= inputEvent.options[inputEvent.selectedIndex].text + '--';
+    errorMessage.textContent= inputEvent.options[inputEvent.selectedIndex].text + '+--';
     inputEvent.blur();
 });
 
